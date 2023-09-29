c5b845369089c3613c515b2063b227d938aaf514
Lucas <fernal13@lsbu.ac.uk>
2023-09-29 22:44:58 +0100

    Added user selection with mobile styling Added icons folder with icons

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { UserSelectionComponent } from './pages/user-selection/user-selection.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'user-selection',
+    component: UserSelectionComponent
   }
 ];
 
