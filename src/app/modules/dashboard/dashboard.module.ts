cc7ad60f79fc34a95dea94e15e23254a7c0bbc45
Lucas <fernal13@lsbu.ac.uk>
2023-10-01 23:19:15 +0100

    Added component declarations Added links to components to routes

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { UserSelectionComponent } from './pages/user-selection/user-selection.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    UserSelectionComponent
+  ],
   imports: [
     CommonModule,
     DashboardRoutingModule
