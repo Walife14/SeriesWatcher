d0abe3b16fccaf96ecb56e26ced95bb9ce8911b4
Lucas <fernal13@lsbu.ac.uk>
2023-10-01 23:14:16 +0100

    Added sign page content and styling Added route to login page Added link from landing page to login with button

--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
+import { SignComponent } from './pages/sign/sign.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LandingComponent
+  },
+  {
+    path: 'login',
+    component: SignComponent
   }
 ];
 
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { LandingComponent } from './pages/landing/landing.component';
+import { SignComponent } from './pages/sign/sign.component';
 
 
 @NgModule({
   declarations: [
-    LandingComponent
+    LandingComponent,
+    SignComponent
   ],
   imports: [
     CommonModule,
