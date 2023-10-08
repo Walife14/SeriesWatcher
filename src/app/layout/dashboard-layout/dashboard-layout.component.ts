65a8f7b7b79805f64bedfef4eb4ad65f56f219b9
Lucas <fernal13@lsbu.ac.uk>
2023-10-08 17:57:36 +0100

    FIXED styling on dashboard layout for nav menu Styling now allows for a better visual experience

--- src/app/layout/dashboard-layout/dashboard-layout.component.ts
+++ src/app/layout/dashboard-layout/dashboard-layout.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, HostListener } from '@angular/core';
 })
 export class DashboardLayoutComponent implements OnInit {
 
-  navOpen: boolean = false;
+  navOpen: boolean = true;
 
   constructor() { }
 
